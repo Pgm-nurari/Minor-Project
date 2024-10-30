6dcb3c50ded647cbfd2747e14a896dab575a3416
Pgm-nurari <mrprogrammer5879@gmail.com>
2024-10-30 15:55:26 +0530

    Adding a user database

--- app.py
+++ app.py
@@ -84,3 +84,48 @@ def admin():
         },
     ]
     return render_template('admin_dash.html',events=events, users=users)
+
+@app.route('/event-manager-dashboard')
+def event_manager():
+    users = {
+        "id" : "1234",
+         "username": "John Doe", 
+         "email": "john@example.com", 
+         "role": "Event Manager",
+         "department": "Commerce",
+         "event" :  "Annual Conference",
+         "phone" : "2136547895",
+         "sub_event" : ""
+        }
+    
+    events = [
+        {
+            "event_name": "Annual Conference",
+            "status": "Active",
+            "type": "Conference",
+            "department": "Marketing",
+            "duration": "2 Days"
+        },
+        {
+            "event_name": "Workshop on AI",
+            "status": "Upcoming",
+            "type": "Workshop",
+            "department": "IT",
+            "duration": "1 Day"
+        },
+        {
+            "event_name": "Annual Conference",
+            "status": "Active",
+            "type": "Conference",
+            "department": "Marketing",
+            "duration": "2 Days"
+        },
+        {
+            "event_name": "Workshop on AI",
+            "status": "Upcoming",
+            "type": "Workshop",
+            "department": "IT",
+            "duration": "1 Day"
+        },
+    ]
+    return render_template('eventmanager_dash.html',events=events, user=users)
